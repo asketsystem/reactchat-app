8e48606c66b978bc1e493f08513d33e697f6be5d
asketsystem <asketsystem1@gmail.com>
2019-06-30 22:43:14 +0000

    Update
    reactApp

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,16 @@
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
-
 var config = {
-    apiKey: "AIzaSyCwf42LOyV7ym0_3iNfBtB51ZczShX-FIA",
-    authDomain: "reactchat-app-42b22.firebaseapp.com",
-    databaseURL: "https://reactchat-app-42b22.firebaseio.com",
-    projectId: "reactchat-app-42b22",
-    storageBucket: "reactchat-app-42b22.appspot.com",
-    messagingSenderId: "905107932189"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyCwf42LOyV7ym0_3iNfBtB51ZczShX-FIA",
+  authDomain: "reactchat-clone.firebaseapp.com",
+  databaseURL: "https://reactchat-clone.firebaseio.com",
+  projectId: "reactchat-app-42b22",
+  storageBucket: "reactchat-app-42b22.appspot.com",
+  messagingSenderId: "905107932189"
+};
+firebase.initializeApp(config);
 
-
-  export default firebase;
+export default firebase;
